783255c4cbc624978e9090866b426d43c7eef3ac
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2023-02-06 11:39:38 +0900

    add:Prisma Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TodoModule } from './todo/todo.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule, TodoModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    AuthModule,
+    TodoModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
