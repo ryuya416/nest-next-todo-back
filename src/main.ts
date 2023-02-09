170ccfb2c305b6dcdb78c36eb55e984edf2ba0f9
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2023-02-09 10:22:45 +0900

    CSRF Token

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,28 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Request } from 'express';
 import * as cookieParser from 'cookie-parser';
-import * as csruf from 'csurf';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     credentials: true,
-    origin: ['https://localhost:3000'],
+    origin: ['http://localhost:3000'],
   });
   app.use(cookieParser());
-  await app.listen(3005);
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
